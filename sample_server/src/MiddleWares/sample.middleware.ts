d962e2d87e71433bf17ef0beeb29fb4a1507f069
riyazahmed001 <rafiriyazahmed@gmail.com>
2022-12-03 15:56:51 +0530

    adding additional docker commands

--- sample_server/src/MiddleWares/sample.middleware.ts
+++ sample_server/src/MiddleWares/sample.middleware.ts
@@ -2,7 +2,7 @@ var express = require('express')
 
 export class SampleMiddleWare {
     public static myLogger = function (req, res, next) {
-        console.log('LOGGED')
+        console.log('middleware called')
         next();
     }
 }
